51e26ac2b725e2e88c2db9eb4912810df5768f95
Justin McKay <juzzbott@gmail.com>
2014-07-27 20:40:01 +1000

    Added the base line project for Assignment 1 - Librarian project.

---
+++ Assignment 1/Librarian/Interfaces/Daos/IMemberDAO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Librarian.Interfaces.Entities;
+using Librarian.Interfaces.Helpers;
+
+namespace Librarian.Interfaces.Daos
+{
+    interface IMemberDAO
+    {
+        IMember addMember(string firstName, string lastName, string ContactPhone, string emailAddress);
+
+        IMember getMemberByID(int id);
+
+        List<IMember> listMembers();
+
+        List<IMember> findMembersByLastName(string lastName);
+
+        List<IMember> findMembersByEmailAddress(string emailAddress);
+
+        List<IMember> findMembersByNames(string firstName, string lastName);
+
+    }
+}
---
+++ Assignment 1/Librarian/Interfaces/Helpers/IBookHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Librarian.Interfaces.Entities;
+
+namespace Librarian.Interfaces.Helpers
+{
+    interface IBookHelper
+    {
+        IBook makeBook(string author, string title, string callNumber, int id);
+
+    }
+}
