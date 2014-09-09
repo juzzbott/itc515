c9513fde6953eccca3bb69b17f62523f6e8ff47e
Justin McKay <juzzbott@gmail.com>
2014-09-10 07:32:38 +1000

    Created the CrownAndAnchorGame.Tests unit test project and added to the solution. Wrote and tested the UAT003 – Bug_Test_Player_Cannot_Reach_Betting_Limit unit test.

---
+++ Assignment 2/Source/CrownAndAnchorGame.Tests/UATBugTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using CrownAndAnchorGame;
+
+namespace CrownAndAnchorGame.Tests
+{
+	[TestClass]
+	public class UATBugTests
+	{
+		[TestMethod]
+		[TestCategory("BUG002")]
+		public void Bug_Test_Player_Cannot_Reach_Betting_Limit()
+		{
+
+			// Create the bet amount
+			int amount = 5;
+			int initialBalance = 5;
+
+			// Create the Player object
+			Player player = new Player("Tester", initialBalance);
+			bool exceededLimit = player.balanceExceedsLimitBy(amount);
+
+			Assert.IsFalse(exceededLimit, "The bug no longer exists as the balance can be the limit of the betting.");
+
+		}
+	}
+}
