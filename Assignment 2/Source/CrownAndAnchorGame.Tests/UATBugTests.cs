305ed6031e6682e7205efbfaeeda8f64592458b5
Justin McKay <juzzbott@gmail.com>
2014-09-10 07:50:55 +1000

    Completed test definition for UAT004 – Resolve_Test_Player_Can_Now_Reach_Betting_Limit. Implemented fix as outlined in the Debugging Log document for BUG002. Validated bug has been resolved with the UAT004 – Resolve_Test_Player_Can_Now_Reach_Betting_Limit unit test. Updated the Unit Test Specifications document to reflect these results.

--- Assignment 2/Source/CrownAndAnchorGame.Tests/UATBugTests.cs
+++ Assignment 2/Source/CrownAndAnchorGame.Tests/UATBugTests.cs
@@ -24,5 +24,22 @@ namespace CrownAndAnchorGame.Tests
 			Assert.IsFalse(exceededLimit, "The bug no longer exists as the balance can be the limit of the betting.");
 
 		}
+
+		[TestMethod]
+		[TestCategory("BUG002")]
+		public void Resolve_Test_Player_Can_Now_Reach_Betting_Limit()
+		{
+
+			// Create the bet amount
+			int amount = 5;
+			int initialBalance = 5;
+
+			// Create the Player object
+			Player player = new Player("Tester", initialBalance);
+			bool exceededLimit = player.balanceExceedsLimitBy(amount);
+
+			Assert.IsTrue(exceededLimit, "The bug still exists as the balance cannot reach the limit of the betting.");
+
+		}
 	}
 }
